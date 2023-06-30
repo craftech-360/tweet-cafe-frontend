cc6c383f2d28080c455159938773ba6c75edfff4
s-a-c-1-8 <Sac@8teen.in>
2023-06-30 22:43:05 +0530

    uopdaed changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Socket } from 'ngx-socket-io';
 import { RxQueue } from 'rx-queue';
 import { DelayQueue } from 'rx-queue';
 import { count } from 'letter-count';
+import * as moment from 'moment';
+import * as momentTimeZonw from 'moment-timezone';
+import { Observable, Subject, concat, concatMap, delay, from, ignoreElements, mapTo, of, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,71 +13,172 @@ import { count } from 'letter-count';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  profilePic = "../assets/images/no-img.png"
   title = 'tweet-frontend';
-  previousJson=[]
+  previousJson = []
   list = [];
-  selectedTweet:any = signal({})
-  key:any = ''
-  a:boolean = true;
+  selectedTweet: any = signal({})
+  key: any = ''
+  a: boolean = true;
+  delay: any;
+  latestDate: any = moment().subtract(45, 'seconds').format('YYYY-MM-DDTHH:mm:ssZ');
 
   @HostListener('document:keypress', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) { 
+  handleKeyboardEvent(event: KeyboardEvent) {
     this.key = event.key;
-    if(this.key == 'n'){
-    // this.selectedTweet = this.list.shift();
-    this.socket.emit('getTweets');
+    if (this.key == 'n') {
+      // this.selectedTweet = this.list.shift();
+      this.socket.emit('getTweets');
     }
   }
-  
-  constructor(private socket : Socket) { }
+
+  constructor(private socket: Socket) { }
 
   ngOnInit(): void {
 
-    const delay = new DelayQueue(15000)
-    
-    // set delay period time to 500 milliseconds
-    delay.subscribe((x:any)=>{
-      var i = this.list.indexOf(x);
-      this.list.splice(i,1);
-      this.selectedTweet.set(x);
+    this.delay = new DelayQueue(15000)
 
-      if(count(x.text)['chars']<60){
-        console.log('less');
-        this.socket.emit("invalid")
+    let subject = new Subject();
+    let period = 1500;
+
+    function createDelayQueue(data: any): Observable<any> {
+      return from(data).pipe(
+        concatMap((data) => timer(1000).pipe(delay(15000), mapTo(data)))
+      );
+    }
+
+    // const data = [2000, 3000, 1000];
+
+
+
+
+    // subject.pipe(
+    //   concatMap(x => concat(
+    //     of(x),      
+    //     timer(period).pipe(
+    //       ignoreElements(),
+    //     ),
+    //   )),
+    // ).subscribe((item: any) => subject.next(item))
+
+
+    // set delay period time to 500 milliseconds
+    // this.delay.subscribe((x: any) => {
+    //   console.log(this.list.length);
+    //   var i = this.list.indexOf(x);
+    //   this.list.splice(i, 1);
+
+    //   this.selectedTweet.set(x);
+    //   console.log(x._id, "id");
+
+    //   if (count(x.text)['chars'] < 60) {
+    //     console.log('less');
+    //     this.socket.emit("invalid", x._id)
+    //   }
+    //   else {
+    //     this.socket.emit("sendCommand", x._id)
+    //   }
+
+    //   if (this.list.length == 0) {
+    //     this.delay.complete();
+    //     console.log("delay complete");
+    //     this.delay.unsubscribe();
+    //     console.log("delay unsubscribed");
+
+    //     createNewdelay();
+    //   }
+
+    // }, (err) => {
+    //   console.log(err.message);
+    // }, () => {
+    //   console.log("complete");
+    //   this.socket.emit('getTweets', this.latestDate);
+    // }).add(() => {
+    //   // this.socket.emit('getTweets', this.latestDate)
+    // });
+
+    this.socket.on("tweets", (data: []) => {
+
+
+      //delay= new DelayQueue(5000);
+      // console.log(e);
+      if (data.length != 0) {
+        this.list = [];
+        console.log("data");
+        console.log(data);
+        this.list = data;
+        let tweet = this.list.map(tweet => moment(tweet.created_at));
+        this.latestDate = moment.max(tweet).format('YYYY-MM-DDTHH:mm:ssZ');
+        //this.latestDate = moment(this.latestDate).tz('Asia/Kolkata').format('YYYY-MM-DDTHH:mm:ssZ');
+        console.log("checking list");
+        let tmp = [...this.list];
+        console.log(tmp);
+        const delayQueue$ = createDelayQueue(data);
+        delayQueue$.subscribe(
+          (x) => {
+            var i = this.list.indexOf(x);
+            this.list.splice(i, 1);
+            this.selectedTweet.set(x);
+            if (count(x.text)['chars'] < 60) {
+              console.log('less');
+              this.socket.emit("invalid", x._id)
+            }
+            else {
+              this.socket.emit("sendCommand", x._id)
+            }
+            console.log(x);
+            console.log("Hell");
+            // console.log(Delay ${ms} completed.);
+          },
+          null,
+          () => {
+            console.log('All data completed!');
+            this.socket.emit('getTweets', this.latestDate);
+          }
+        );
+        // tmp.forEach(x => {
+        //   this.delay.next(x);
+        //   if (tmp.indexOf(x) == tmp.length - 1) {
+        //     // delay.complete();
+        //     // delay.unsubscribe();
+        //   }
+        // });
+        //console.log(tmp.length * 15000);
+        // delay.complete();
       }
-      else{
-        this.socket.emit("sendCommand")
+      else {
+        console.log("hello");
+        this.latestDate = moment().subtract(45, 'seconds').format('YYYY-MM-DDTHH:mm:ssZ');
+        setTimeout(() => {
+          this.socket.emit('getTweets', this.latestDate);
+        }, 30000);
       }
-      
-    });
-    
-    this.socket.on("tweets",(e:any)=>{
-       console.log(e);
-      this.list=e;
-      let tmp= [...this.list];
-      console.log(tmp);
-      tmp.forEach(x=>{
-        delay.next(x);   
-      });
+
 
       //this.list.pop();
-      
+
       // this.list = e
       // if(e){
-        // this.list.push(e);
+      // this.list.push(e);
       //   console.log(this.list);
-        
+
       //   if(this.a){
       //     this.selectedTweet =  this.list.shift();
       //     console.log(this.selectedTweet,"main");
-          
+
       //     this.socket.emit('sendCommand');
       //     this.a = false;
       //   }
       // }
     })
-    
+
 
   }
 
 }
+function createNewdelay() {
+  console.log("new Delay");
+  console.log(this.delay)
+  this.delay = new DelayQueue(15000);
+}
+
